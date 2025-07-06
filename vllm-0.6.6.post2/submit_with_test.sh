40d3bc8d7d2e384cef45a928d8330b3500e736e4
Thomas Brettin <brettin@anl.gov>
2025-07-06 14:33:09 +0000

    OFFSET working

--- vllm-0.6.6.post2/submit_with_test.sh
+++ vllm-0.6.6.post2/submit_with_test.sh
@@ -9,11 +9,18 @@
 #PBS -l filesystems=flare:home
 #PBS -l place=scatter
 
+#mkdir -p /tmp/brettin/copper
+#module load copper
+#launch_copper.sh
+
+#####################################################
+# Set OFFSET if you want to resume processing files #
+# from where you left off.                          #
+#####################################################
+
 SCRIPT_DIR="/lus/flare/projects/candle_aesp_CNDA/brettin/Aurora-Inferencing/vllm-0.6.6.post2"
 cat "$PBS_NODEFILE" > $SCRIPT_DIR/hostfile
 
-# module load copper
-# launch_copper.sh
 
 # Function to start vLLM on a host
 start_vllm_on_host() {
@@ -29,34 +36,44 @@ start_vllm_on_host() {
     fi
 }
 
-# Create an array containing hostnames.
-mapfile -t hosts < <(cut -d'.' -f1 "$PBS_NODEFILE")
 
-# Create an array containing gene filenames.
+# Create arrays containing hostnames and filenames.
+mapfile -t hosts < <(cut -d'.' -f1 "$PBS_NODEFILE")
 filenames=(${SCRIPT_DIR}/../examples/TOM.COLI/batch_1/genes/*)
 
-# TODO:
-OFFSET=2 # number of files processed
 
-if (( ${#hosts[@]} < ${#filenames[@]} )); then
-    min=${#hosts[@]}
+# Loop over the smaller of hostnames or filenames with an OFFSET option for restarting.
+OFFSET=0 # number of files already processed
+total_files=$(( ${#filenames[@]} - OFFSET ))
+total_hosts=${#hosts[@]}
+
+if (( ${total_hosts} < ${total_files} )); then
+    min=${total_hosts}
     echo "$(date) min = ${min}"
 else
-    min=${#filenames[@]}
+    min=${total_files}
     echo "$(date) min = ${min}"
 fi
 
 declare -a pids
-for ((i = ${OFFSET}; i < ${min}; i++)); do
-    echo "$(date) processing genes in ${filenames[i]} on host ${hosts[i]}"
-    start_vllm_on_host ${hosts[i]} ${filenames[i]} &
+for ((i = OFFSET; i < min + OFFSET; i++)); do
+    index=$((i - OFFSET)) # for indexing the hosts
+    file="${filenames[i]}"
+    host="${hosts[index]}"
+
+    echo "$(date) processing genes in ${file} on host ${host}"
+    start_vllm_on_host ${host} ${file} &
     pid=$!
     pids+=($pid)
 done
 
+
+# Wait for the jobs to finish.
 for pid in "${pids[@]}"; do
     echo "$(date) Waiting for PID: $pid"
     if wait $pid; then
         echo "$(date) Process $pid completed successfully"
+    else
+        echo "$(date) Process $pid FAILED with exit code $?"
     fi
 done
