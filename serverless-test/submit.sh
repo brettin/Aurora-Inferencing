04094bda37b9a56083cafc4c2163d29b29668214
Thomas Brettin <brettin@anl.gov>
2025-08-05 20:40:46 +0000

    fixed SCRIPT_DIR location

--- serverless-test/submit.sh
+++ serverless-test/submit.sh
@@ -10,13 +10,8 @@
 
 SCRIPT_DIR="/lus/flare/projects/candle_aesp_CNDA/brettin/Aurora-Inferencing/serverless-test"
 
-# Num nodes
 NN=$(cat $PBS_NODEFILE | wc -l)
-
-# Num processes
 NP=$(( NN * 12 ))
-
-# Processes per node
 PPN=12
-
+echo "mpiexec -np $NP -ppn $PPN ${SCRIPT_DIR}/gpu_tile_compact.sh ${SCRIPT_DIR}/simple_test.sh"
 mpiexec -np $NP -ppn $PPN ${SCRIPT_DIR}/gpu_tile_compact.sh ${SCRIPT_DIR}/simple_test.sh
