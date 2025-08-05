00c93037e5057fb9eb001c3a14e82031890bf1ea
Thomas Brettin <brettin@anl.gov>
2025-08-05 20:20:27 +0000

    manage input and output files

--- serverless-test/simple_test.py
+++ serverless-test/simple_test.py
@@ -1,18 +1,26 @@
+import sys
 from vllm import LLM, SamplingParams
 
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
+if len(sys.argv) == 3:
+    infile=sys.argv[1]
+    outfile=sys.argv[2]
+else:
+    print(f"usage $sys.argv[0] infile outfile")
+
+prompts = []
+
+with open(infile, "r") as f:
+    for line in f:
+        clean = line.strip()
+        if clean:
+            prompts.append(clean)
+
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-
 llm = LLM(model="facebook/opt-125m", device="xpu")
-
 outputs = llm.generate(prompts, sampling_params)
 
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+with open(outfile, "w") as f:
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        f.write(f"Prompt: {prompt!r}, Generated text: {generated_text!r}\n\n")
