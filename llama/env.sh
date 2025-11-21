2a38b5728178b07e1b584d953208d162c255aad3
Thomas Brettin <brettin@anl.gov>
2025-11-21 20:17:50 +0000

    update readme and env.sh

---
+++ llama/env.sh
@@ -0,0 +1,6 @@
+export HTTP_PROXY=http://proxy.alcf.anl.gov:3128
+export HTTPS_PROXY=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+export https_proxy=http://proxy.alcf.anl.gov:3128
+
+module load cmake
