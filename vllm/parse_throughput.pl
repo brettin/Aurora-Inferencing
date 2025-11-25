65383d484eb5a2e1c10b21226dee5c2f510a2e67
Thomas Brettin <brettin@anl.gov>
2025-11-25 23:16:42 +0000

    add arg for filename

--- vllm/parse_throughput.pl
+++ vllm/parse_throughput.pl
@@ -1,4 +1,5 @@
+$fn=shift @ARGV;
 
 while(<>){
-	print "$1\t$2\n" if /Avg prompt throughput: (\d+\.\d+) tokens\/s, Avg generation throughput: (\d+\.\d+) tokens\/s, Running: (\d+) reqs, Waiting: (\d+) reqs, GPU KV cache usage: (\d+\.\d+)\%, Prefix cache hit rate: (\d+\.\d+)\%/;
+	print "$fn\t$1\t$2\n" if /Avg prompt throughput: (\d+\.\d+) tokens\/s, Avg generation throughput: (\d+\.\d+) tokens\/s, Running: (\d+) reqs, Waiting: (\d+) reqs, GPU KV cache usage: (\d+\.\d+)\%, Prefix cache hit rate: (\d+\.\d+)\%/;
 }
