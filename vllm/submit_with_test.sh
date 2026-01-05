25e4956789f00ec990f1737a7a007ddd9eae0176
Thomas Brettin <brettin@anl.gov>
2026-01-05 18:19:26 +0000

    add redis config

--- vllm/submit_with_test.sh
+++ vllm/submit_with_test.sh
@@ -17,13 +17,20 @@
 SCRIPT_DIR="/lus/flare/projects/candle_aesp_CNDA/brettin/Aurora-Inferencing/vllm"
 cat "$PBS_NODEFILE" > $SCRIPT_DIR/hostfile
 
+# Redis Service Registry Configuration
+source $SCRIPT_DIR/../redis/set_redis_host.sh
+REDIS_HOST=${REDIS_HOST:-localhost}
+REDIS_PORT=${REDIS_PORT:-6379}
+echo "$(date) Redis Service Registry Configuration: REDIS_HOST=${REDIS_HOST}, REDIS_PORT=${REDIS_PORT}"
 
 # Function to start vLLM on a host
 start_vllm_on_host() {
     local host=$1
     local filename=$2
+    local redis_host=$3
+    local redis_port=$4
     
-    if ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no "$host" "cd $SCRIPT_DIR && ./start_vllm_with_test.sh $filename" 2>&1 ; then
+    if ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no "$host" "cd $SCRIPT_DIR && ./start_vllm_with_test.sh $redis_host $redis_port $filename" 2>&1 ; then
         return 0
     else
         echo "$(date) Failed to launch vLLM on $host"
@@ -38,7 +45,7 @@ filenames=(${SCRIPT_DIR}/../examples/TOM.COLI/batch_1/*)
 
 
 # Loop over the smaller of hostnames or filenames with an OFFSET option for restarting.
-OFFSET=1024 # number of files already processed
+OFFSET=3072 # number of files already processed
 total_files=$(( ${#filenames[@]} - OFFSET ))
 total_hosts=${#hosts[@]}
 
@@ -53,6 +60,7 @@ mpicc -o cptotmp ${SCRIPT_DIR}/../cptotmp.c
 export MPIR_CVAR_CH4_OFI_ENABLE_MULTI_NIC_STRIPING=1
 export MPIR_CVAR_CH4_OFI_MAX_NICS=4
 time mpiexec -ppn 1 --cpu-bind numa ./cptotmp /flare/datasets/model-weights/hub/models--meta-llama--Llama-3.3-70B-Instruct
+time mpiexec -ppn 1 --cpu-bind numa ./cptotmp /home/brettin/candle_aesp_CNDA/brettin/Aurora-Inferencing/redis/wheelhouse
 
 declare -a pids
 for ((i = OFFSET; i < min + OFFSET; i++)); do
@@ -61,7 +69,7 @@ for ((i = OFFSET; i < min + OFFSET; i++)); do
     host="${hosts[index]}"
 
     echo "$(date) processing genes in ${file} on host ${host}"
-    start_vllm_on_host ${host} ${file} &
+    start_vllm_on_host ${host} ${file} ${REDIS_HOST} ${REDIS_PORT} &
     pid=$!
     pids+=($pid)
 done
