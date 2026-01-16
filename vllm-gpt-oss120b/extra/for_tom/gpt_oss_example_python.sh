5d6c9536b3696fc2bca82ea78ee581caa03215f4
Thomas Brettin <brettin@anl.gov>
2026-01-16 15:32:12 +0000

    Please enter the commit message for your changes. Lines starting deleted files

--- vllm-gpt-oss120b/extra/for_tom/gpt_oss_example_python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-#
-#
-BENCH_DIR=/lus/flare/projects/datasets/softwares/testing/vllm-efforts
-source ${BENCH_DIR}/env_gpt_oss_120b_12_03_2025.sh
-
-OCL_ICD_FILENAMES="libintelocl.so" VLLM_DISABLE_SINKS=1 python ${BENCH_DIR}/for_tom/example_gpt_oss_120b.py
