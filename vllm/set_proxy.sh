1a91f59d4924fb7c359cff9794f31babb8a739dd
Thomas Brettin <brettin@anl.gov>
2026-02-13 00:57:05 +0000

    first commit

---
+++ vllm/set_proxy.sh
@@ -0,0 +1,4 @@
+export HTTP_PROXY=http://proxy.alcf.anl.gov:3128
+export HTTPS_PROXY=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+export https_proxy=http://proxy.alcf.anl.gov:3128
